830e15d75ac5da00ffe42f684277b3a2226b1fce
DanielBerrio0 <163582475+DanielBerrio0@users.noreply.github.com>
2025-09-13 00:58:11 +0000

    ...

--- controllers/futbol_controller.py
+++ controllers/futbol_controller.py
@@ -3,46 +3,72 @@ from repository.futbol_repository import PaisRepository
 from config.database import get_db_session
 
 futbol_bp = Blueprint('futbol_bp', __name__)
-pais_repo = PaisRepository(db_session)
+
 
 @futbol_bp.route('/futbol', methods=['GET'])
 def get_paises():
+
+    db_session = get_db_session()
+    pais_repo = PaisRepository(db_session)
+    
     paises = pais_repo.get_all_paises()
     resultado = [{'id': p.id, 'nombre_pais': p.nombre_pais} for p in paises]
+    
     return jsonify(resultado), 200
+
 
 @futbol_bp.route('/futbol/<int:pais_id>', methods=['GET'])
 def get_pais(pais_id):
+
+    db_session = get_db_session()
+    pais_repo = PaisRepository(db_session)
+    
     pais = pais_repo.get_pais_by_id(pais_id)
     if not pais:
         return jsonify({'error': 'La selección no se encontró'}), 404
+    
     return jsonify({'id': pais.id, 'nombre_pais': pais.nombre_pais}), 200
+
 
 @futbol_bp.route('/futbol', methods=['POST'])
 def create_pais():
     data = request.json
     if not data or 'nombre_pais' not in data:
         return jsonify({'error': 'Bad request, falta nombre_pais'}), 400
+    
 
+    db_session = get_db_session()
+    pais_repo = PaisRepository(db_session)
+    
     nuevo_pais = pais_repo.create_pais(data['nombre_pais'])
     return jsonify({'id': nuevo_pais.id, 'nombre_pais': nuevo_pais.nombre_pais}), 201
+
 
 @futbol_bp.route('/futbol/<int:pais_id>', methods=['PUT'])
 def update_pais(pais_id):
     data = request.json
     if not data or 'nombre_pais' not in data:
         return jsonify({'error': 'Bad request'}), 400
+    
 
+    db_session = get_db_session()
+    pais_repo = PaisRepository(db_session)
+    
     pais = pais_repo.update_pais(pais_id, data['nombre_pais'])
     if not pais:
         return jsonify({'error': 'País no encontrado'}), 404
-
+    
     return jsonify({'id': pais.id, 'nombre_pais': pais.nombre_pais}), 200
+
 
 @futbol_bp.route('/futbol/<int:pais_id>', methods=['DELETE'])
 def delete_pais(pais_id):
+
+    db_session = get_db_session()
+    pais_repo = PaisRepository(db_session)
+    
     pais = pais_repo.delete_pais(pais_id)
     if not pais:
         return jsonify({'error': 'País no encontrado'}), 404
-
+    
     return jsonify({'result': 'País eliminado'}), 200
